8d9294083c7c7489d22396d3bb304c8622f9aee3
emilime93 <emil93@gmail.com>
2018-03-03 15:31:24 +0100

    Started List-functions

--- introduction.ex
+++ introduction.ex
@@ -7,7 +7,7 @@ defmodule Introduction do
     end
 
     # Product with addition, case
-    def case_product(m, n) do
+    def product_case(m, n) do
         case m do
             0 ->
                 0
@@ -16,9 +16,52 @@ defmodule Introduction do
         end
     end
 
-    #Exponential function
+    # Exponential function
     def exp(_, 0) do 1 end
     def exp(x, y) do
         product(x, exp(x, y-1))
+    end
+
+    # Exponential with case...
+    def exp_case(x, y) do
+        case y do
+            0 ->
+                1
+            _ ->
+                product(x, exp_case(x, y-1))
+        end
+    end
+
+    # Faster exponential
+    def exp_fast(x, 1) do x end
+    def exp_fast(x, y) do
+        case rem(y, 2) do
+            0 ->
+                exp_fast(x, div(y,2)) * exp_fast(x, div(y,2))
+            1 ->
+                exp_fast(x, y-1) * x
+        end
+    end
+
+    # ================ LISTS ================ #
+    # nth element
+    def nth(_, []) do nil end
+    def nth(1, [h|t]) do h end
+    def nth(n, [h|t]) do
+        nth(n-1, t)
+    end
+
+    # len
+    def len([]) do 0 end
+    def len([h|t]) do
+        1 + len(t)
+    end
+    # len with acc
+    def len_acc([]) do 0 end
+    def len_acc(list) do
+        len
+    end
+    def len_acc([h|t], acc) do
+        
     end
 end
